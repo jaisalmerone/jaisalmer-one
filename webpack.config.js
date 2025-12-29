6bcb093f266c4eb1a2c28e17acf78dad1e53690d
AKSHYA MALHOTRA <akshyamalhotra@AKSHYAs-MacBook-Air.local>
2025-12-29 15:31:00 -0600

    Fix Vercel deployment for React Router

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,34 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "production",
   entry: "./src/index.jsx",
-
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/",
+    publicPath: "/",   // 🔥 REQUIRED for Vercel
+    clean: true
   },
-
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    port: 8081,
-    historyApiFallback: true,
-    hot: true,
+  resolve: {
+    extensions: [".js", ".jsx"]
   },
-
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: "babel-loader"
       },
-
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
-
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-
-  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html" // 🔥 THIS CREATES dist/index.html
+    })
+  ]
 };
